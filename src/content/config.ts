f1658490db3ce006afd6d2e85071d68e55758a1b
imade <imadetassaoui@gmail.com>
2023-09-28 17:17:27 +0100

    fixed images issue

--- src/content/config.ts
+++ src/content/config.ts
@@ -4,12 +4,13 @@ import { z, defineCollection } from "astro:content";
 // 2. Define a `type` and `schema` for each collection
 const postsCollection = defineCollection({
   // type: "content", // v2.5.0 and later
-  schema: z.object({
-    author: z.string(),
-    date: z.string(),
-    image: z.string(),
-    title: z.string(),
-  }),
+  schema: ({ image }) =>
+    z.object({
+      author: z.string(),
+      date: z.string(),
+      image: image(),
+      title: z.string(),
+    }),
 });
 
 // 3. Export a single `collections` object to register your collection(s)
