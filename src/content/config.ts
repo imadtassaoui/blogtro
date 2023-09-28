22bf43c17e09053cafd4ee25db707b1caf96ac54
imade <imadetassaoui@gmail.com>
2023-09-28 17:05:18 +0100

    fixed collections schema

--- src/content/config.ts
+++ src/content/config.ts
@@ -2,16 +2,17 @@
 import { z, defineCollection } from "astro:content";
 
 // 2. Define a `type` and `schema` for each collection
-const blogCollection = defineCollection({
-  type: "content", // v2.5.0 and later
+const postsCollection = defineCollection({
+  // type: "content", // v2.5.0 and later
   schema: z.object({
+    author: z.string(),
+    date: z.string(),
+    image: z.string(),
     title: z.string(),
-    tags: z.array(z.string()),
-    image: z.string().optional(),
   }),
 });
 
 // 3. Export a single `collections` object to register your collection(s)
 export const collections = {
-  blog: blogCollection,
+  posts: postsCollection,
 };
